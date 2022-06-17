5c0b44f4078355476a7342fc0ce959dc2e70ae83
Yevhenii <zhtsibatyuk@gmail.com>
2022-06-17 09:58:20 +0300

    create class WorkOn

--- src/test/java/EvheniisWorckSpace/WorkOn.java
+++ src/test/java/EvheniisWorckSpace/WorkOn.java
@@ -1,2 +1,14 @@
-package EvheniisWorckSpace;public class WorkOn {
+package EvheniisWorckSpace;
+
+import HomeWorks.AmazonPageObjects;
+import HomeWorks.TestInit;
+import org.testng.annotations.Test;
+
+@Test
+public class WorkOn extends TestInit {
+    public void amazonShoping(){
+        AmazonPageObjects amazonPageObjects = new AmazonPageObjects(driver);
+        amazonPageObjects.goToAmazon();
+
+    }
 }
