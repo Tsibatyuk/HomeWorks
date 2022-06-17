eb5ff9a7b64dcc08ac845b8f27308f0af46e6ab4
Yevhenii <zhtsibatyuk@gmail.com>
2022-06-17 16:45:05 +0300

    create some constructors

--- src/test/java/Tests/JusttestForPractice.java
+++ src/test/java/Tests/JusttestForPractice.java
@@ -1,4 +1,21 @@
 package Tests;
 
 public class JusttestForPractice {
+    Constructors constructors = new Constructors();
+    int population;
+    String planetName;
+    public JusttestForPractice(int population, String planetName){
+        this.population = population;
+        this.planetName = planetName;
+    }
+
+
+    public void ourPlanet(){
+
+        System.out.println(Just(12,"Kaka" ));
+    }
+
+
+
+
 }
