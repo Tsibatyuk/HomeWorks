de09c9b464df1802956ce379c56bd59d911ca87d
Yevhenii <zhtsibatyuk@gmail.com>
2022-06-18 18:04:40 +0300

    constructor edit

--- src/test/java/Tests/JusttestForPractice.java
+++ src/test/java/Tests/JusttestForPractice.java
@@ -6,15 +6,26 @@ public class JusttestForPractice {
     int population;
     String planetName;
 
+    public void planetEarth() {
+        System.out.println("Here we are live");
+    }
+    public void planetMars(){
+        System.out.println("Here we will live");
+    }
 
-
-
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         JusttestForPractice planet = new JusttestForPractice();
         planet.population = 12;
         planet.planetName = "Earth";
-        System.out.println(planet.population+" "+planet.planetName);
+        System.out.println(planet.population + " " + planet.planetName);
+        planet.planetEarth();
+
+        JusttestForPractice planet2 = new JusttestForPractice();
+        planet2.population = 21;
+        planet2.planetName = "Mars";
+        System.out.println(planet2.population+" "+planet2.planetName);
+        planet2.planetMars();
+
 
     }
 }
