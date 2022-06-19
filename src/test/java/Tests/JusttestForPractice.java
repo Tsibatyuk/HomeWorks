4e5cd555f2f32085d210d3affcb1fb853be7537f
Yevhenii <zhtsibatyuk@gmail.com>
2022-06-19 22:31:52 +0300

    make some constructor

--- src/test/java/Tests/JusttestForPractice.java
+++ src/test/java/Tests/JusttestForPractice.java
@@ -1,12 +1,12 @@
 package Tests;
 
-import org.testng.annotations.Test;
-
 public class JusttestForPractice {
     int population;
+    int a, b, c;
     String planetName;
 
     public void planetEarth() {
+
         System.out.println("Here we are live");
     }
     public void planetMars(){
@@ -15,17 +15,26 @@ public class JusttestForPractice {
 
     public static void main(String[] args) {
         JusttestForPractice planet = new JusttestForPractice();
+        JusttestForPractice planet2 = new JusttestForPractice();
+
         planet.population = 12;
         planet.planetName = "Earth";
         System.out.println(planet.population + " " + planet.planetName);
         planet.planetEarth();
 
-        JusttestForPractice planet2 = new JusttestForPractice();
         planet2.population = 21;
         planet2.planetName = "Mars";
         System.out.println(planet2.population+" "+planet2.planetName);
         planet2.planetMars();
 
+
+    }
+    public void first(int a){
+        this.a = a;    }
+    public void second(int b){
+        this.b = b;    }
+    public void third(int c){
+        this.c = c;
 
     }
 }
