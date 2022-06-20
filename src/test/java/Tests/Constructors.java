b0b0cb0cad74b827359a6a8cb368db2bcbd54433
Yevhenii <zhtsibatyuk@gmail.com>
2022-06-20 12:26:12 +0300

    forgot to add

--- src/test/java/Tests/Constructors.java
+++ src/test/java/Tests/Constructors.java
@@ -1,10 +1,14 @@
 package Tests;
 
+import org.testng.annotations.Test;
+
 public class Constructors implements Learning{
 
-
+@Test
     @Override
     public void someInformation() {
+        JusttestForPractice justtestForPractice = new JusttestForPractice();
         System.out.println("have some info for you");
+        justtestForPractice.someInformation();
     }
 }
