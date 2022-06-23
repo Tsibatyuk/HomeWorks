c5b4d627b6cb94bc541fcc377ccaad497343d5f2
Yevhenii <zhtsibatyuk@gmail.com>
2022-06-23 11:08:49 +0300

    add assert

--- src/test/java/Tests/DlyaAndriya.java
+++ src/test/java/Tests/DlyaAndriya.java
@@ -5,6 +5,7 @@ import TestInitPackage.TestInit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DlyaAndriya extends TestInit {
@@ -13,11 +14,11 @@ public class DlyaAndriya extends TestInit {
         VansUkPage vansUkPage = new VansUkPage(driver);
         vansUkPage.goToVasSite();
         vansUkPage.allowedBTN().click();
-        vansUkPage.sectionWomenFromList().getAttribute("text");
-        {
+        vansUkPage.sectionWomenFromList().getAttribute("text");       {
 
-            boolean color = (vansUkPage.sectionWomenFromList().getCssValue("color")) == "rgba(203, 27, 44, 1)";{
-
+            boolean color = (vansUkPage.sectionWomenFromList().getCssValue("color"))==("rgba(203, 27, 44, 1)") ;{
+                Assert.assertEquals(vansUkPage.sectionWomenFromList().getCssValue("color"),"rgba(203, 27, 44, 1)");
+                Assert.assertTrue(color);
                 System.out.println("Color of a button : red");
             }
 
