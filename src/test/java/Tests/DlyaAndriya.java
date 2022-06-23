3e830552fd78843d54e0756f9b97a7c079151681
Yevhenii <zhtsibatyuk@gmail.com>
2022-06-23 11:14:23 +0300

    add assert

--- src/test/java/Tests/DlyaAndriya.java
+++ src/test/java/Tests/DlyaAndriya.java
@@ -16,7 +16,7 @@ public class DlyaAndriya extends TestInit {
         vansUkPage.allowedBTN().click();
         vansUkPage.sectionWomenFromList().getAttribute("text");       {
 
-            boolean color = (vansUkPage.sectionWomenFromList().getCssValue("color"))==("rgba(203, 27, 44, 1)") ;{
+            boolean color = (vansUkPage.sectionWomenFromList().getCssValue("color")).equals("rgba(203, 27, 44, 1)") ;{
                 Assert.assertEquals(vansUkPage.sectionWomenFromList().getCssValue("color"),"rgba(203, 27, 44, 1)");
                 Assert.assertTrue(color);
                 System.out.println("Color of a button : red");
